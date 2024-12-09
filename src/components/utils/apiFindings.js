5ff7442bb1f599be2f96dbaf43958f5d531928f9
polikanov.v <81175797+polikanovv@users.noreply.github.com>
2024-12-09 19:01:07 +0300

    add

---
+++ src/components/utils/apiFindings.js
@@ -0,0 +1,10 @@
+// Объект для передачи(получаения) данных на сервер(сервера)
+const apiFindings = {
+  link: 'https://mesto.nomoreparties.co/v1/frontend-st-cohort-201/',
+  headers: {
+    authorization: '3080b895-a886-4395-a2f7-21b3bdca1836',
+    'Content-Type': 'application/json'
+  }
+}
+// Экспортируем объект в index.js
+export { apiFindings };
